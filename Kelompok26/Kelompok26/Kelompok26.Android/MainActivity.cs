ab859867b327de157459dd4fdf5cbe6b2d3f21f2
perdanafavo <perdanafavo97@gmail.com>
2018-10-23 19:51:42 +0700

    TUGAS MODUL 1

---
+++ Kelompok26/Kelompok26/Kelompok26.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Kelompok26.Droid;
+
+namespace Kelompok26.Droid
+{
+    [Activity(Label = "Kelompok26", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ Kelompok26/Kelompok26/Kelompok26/Model/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Kelompok26
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok26";
+        public static string Password = "Kelompok26";
+    }
+}
